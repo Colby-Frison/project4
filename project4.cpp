6ed53589bf5be4e692edeaaa57009ace274b0c30
Colby-Frison <colbyfrison100@gmail.com>
2024-11-12 17:09:14 -0600

    Added method descriptions

--- project4.cpp
+++ project4.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-Colby frison
+Colby Frison
 CS-2413 ~ Data Structure
 Project4 ~ M-Tree
 
-An M-tree is basically a 2,3 tree. the tree consists of 3 main vairables, 'M', 'values', and 'children'. Starting
-with 'M' it means there are M children per noce, and 'M-1' values per node; so if M=3 then each node contains 2 
+An M-tree is basically a 2,3 tree. the tree consists of 3 main variables, 'M', 'values', and 'children'. Starting
+with 'M' it means there are M children per node, and 'M-1' values per node; so if M=3 then each node contains 2 
 values and has 3 children nodes. The next is 'values' which simply store the values in that given node, its pretty
-much the _info. The last is 'children' it is a vector of Mtree nodes that are the children of the given node. This 
-makes recursion very simple as it makes each node its own tree. 
+much the _info. The last is 'children' it is a vector of M-tree nodes that are the children of the given node. This 
+makes recursion very simple as it makes each node its own tree (method calls work on children nodes as well as roots)
 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -55,30 +55,40 @@ class MTree {
 protected:
     int M; // Maximum number of children per node (M+1 way split)
     vector<DT> values; // Values stored in the node (M-1 values)
-    vector<MTree*> children; // Pointers to child MTrees (M+1 children)
+    vector<MTree*> children; // Pointers to child MTrees (M children) ~ Check on this number cuz there was mixed instructions
+    // the pdf says m-1 for values and m for children, but in the code segments it says m-1 then m+1 so idk
 
 public:
-    // initialize treee, set max nodes per child
+    // initialize tree, set max nodes per child
     MTree(int M) : M(M) {
-
         //know size of vector so reserve size to save time
         values.reserve(M-1);
-        children.reserve(M+1);
+        children.reserve(M); // check this number
     }
 
     // loop through entire tree deleting each
     ~MTree() {
+        // auto just deduces data type automatically, no real difference, but idk
         for (auto child : children) {
             delete child;
         }
     }
 
-    // if the node has no children its a leaf, good dignifier of when to end loop/recursion
+    // if the node has no children its a leaf, good identifier of when to end loop/recursion
     bool is_leaf() const {
-        return children.empty();
+        return children.empty(); // vector method, if empty returns true, this is just a remaster of that
     }
 
-    //insert value into the tree
+    /* * * * * * * * Insert Method * * * * * * * * *
+    Checks if value already exists in the tree, if not move on.abort
+
+    Then check if node is a leaf, is so find where the val goes, then insert
+    if the number of values exceed M-1 go to split node method. This is the 
+    base case that stops the recursion
+
+    if it is not a leaf recursively traverse the method to find the right
+    insert location
+    * * * * * * * * * * * * * * * * * * * * * * * * */
     void insert(const DT& value) {
         // use search to see if value is already in the tree, if so throw exception
         if (search(value)) {
@@ -102,6 +112,14 @@ public:
         }
     }
 
+    /* * * * * * * Split_Node Method * * * * * * * * *
+    Create left and right nodes to split values to, then split original
+    values list in half assigning values to each node.abort
+
+    Middle value is retained, then the original node is cleared and 
+    middle is pushed back into the vector. then children are declared
+
+    * * * * * * * * * * * * * * * * * * * * * * * * */
     void split_node() {
         if (is_leaf()) {
             // Create two new child nodes
@@ -128,6 +146,13 @@ public:
         }
     }
 
+    /* * * * * find_child Method * * * * * * * * *
+    This is a simple helper method in a few different methods.
+    It iterates through the values vector, if the target value is less than the 
+    val in the vector then if it exists it would be in that child. It continues
+    to do that until the end of the vector. If it does not find it it returns the 
+    last child in the children vector.
+    * * * * * * * * * * * * * * * * * * * * * * */
     MTree* find_child(const DT& value) {
         for (size_t i = 0; i < values.size(); i++) {
             if (value < values[i]) {
@@ -137,6 +162,13 @@ public:
         return children.back();
     }
 
+    /* * * * * Search Method * * * * * * * * * * *
+    Checks if leaf, if it is check values vector, if its there return true
+    if not return false. This is the base case of the recursive method.
+
+    Then finds child node where it would be based on pushed value. Then recursive
+    call is made to continue search.
+    * * * * * * * * * * * * * * * * * * * * * * */
     bool search(const DT& value) {
         // If leaf node, search in values
         if (is_leaf()) {
@@ -148,6 +180,18 @@ public:
         return child->search(value);
     }
 
+    /* * * * * Remove Method * * * * * * * * * * *
+    Simple remove method, the first if statement is in regards to leaf nodes.
+    It first uses the helper method to get a pointer to the location, or lack there
+    of, of the value. if its pointing to the end of the vector, that means value
+    is greater than all vals in the vector, meaning its not in it, the second statement
+    checks to see if the value being pointed to is == to the value. These are both crucial
+    in keeping everything defined and correct. it those are both false it deletes the value 
+    that position in the values vector.
+
+    If its note a leaf it uses the find_child to get the proper child node, then makes a 
+    recursive call to repeat the above process in teh child node.
+    * * * * * * * * * * * * * * * * * * * * * * */
     void remove(const DT& value) {
         if (is_leaf()) {
             auto it = custom_lower_bound(values.begin(), values.end(), value);
@@ -155,7 +199,7 @@ public:
                 throw NotFoundException();
             }
             values.erase(it);
-            cout << "The value = " << value << " has been removed." << endl;
+            cout << "The value = " << value << " has been removed." << endl; // change this
         } else {
             MTree* child = find_child(value);
             child->remove(value);
@@ -201,6 +245,14 @@ public:
         cout << "The tree has been rebuilt." << endl;
     }
 
+    /* * * * * Collect Values Method * * * * * *
+    This one is also pretty simple. First starts by checking leaf nodes.
+    just uses inbuilt vector functions to add all elements from values vector
+    to allValues vector. 
+
+    Then uses recursion to gather values of child nodes then adds it to allValues
+    Then return it... pretty simple
+    * * * * * * * * * * * * * * * * * * * * * * */
     vector<DT> collect_values() {
         vector<DT> allValues;
         if (is_leaf()) {
@@ -214,18 +266,22 @@ public:
         return allValues;
     }
 
-    // just a search wrapper, check instructions to see if I have to find the index or height or something
+    /* * * * * * * * find Method * * * * * * * * *
+    The find method is basically just a call to the search method with some cout statements.
+    It also returns a bool based on if the value was found.
+    * * * * * * * * * * * * * * * * * * * * * * */
     bool find(DT value) {
         bool found = search(value);
         if (found) {
-            cout << "The element with value = " << value << " was found." << endl;
+            cout << "The element with value = " << value << " was found." << endl; // adjust these
         } else {
             cout << "The element with value = " << value << " not found." << endl;
         }
         return found;
     }
 
-    //just a display method, check instructions to see if theres some special syntax
+    // I have it printing the full list at the end, but I dont think this is useful so probably delete 
+    // if it isn't really needed
     void print_final_list() {
         vector<DT> final_values = collect_values();
         cout << "Final list: ";
